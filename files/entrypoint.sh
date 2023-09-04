a74809cb7ebd5289787a063de3a046795d1b78af
freeyoungmax <141470035+freeyoungmax@users.noreply.github.com>
2023-09-04 17:54:15 +0800

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'2c58399c-ae25-4b22-a0e5-4defff6ff99e'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
